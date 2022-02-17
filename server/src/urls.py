0d6c2b8c04e246339564bbbc6f83fc8daf622dbd
roddelaporte <rodrigod@delaportedigital.com>
2022-02-17 18:19:19 -0300

    CRUD Templates y URLS

--- server/src/urls.py
+++ server/src/urls.py
@@ -18,8 +18,9 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('blog/', include('blog.urls', namespace='blog')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('course/', include('elearning.urls')),
     path('admin/', admin.site.urls),
 ]
