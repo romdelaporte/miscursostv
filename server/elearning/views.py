bcfec6c42479efe6d7655bf4b261f4aa57554daa
roddelaporte <rodrigod@delaportedigital.com>
2022-03-20 19:36:07 -0300

    List View courses

--- server/elearning/views.py
+++ server/elearning/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
@@ -8,7 +9,8 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.forms.models import modelform_factory
 from django.apps import apps
 from .forms import ModuleFormSet
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
+from django.db.models import Count
 
 
 class OwnerMixin(object):
@@ -141,3 +143,22 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'Saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/detail.html'
--- server/src/urls.py
+++ server/src/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from elearning.views import CourseListView
+
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('blog/', include('blog.urls', namespace='blog')),
     path('course/', include('elearning.urls')),
     path('admin/', admin.site.urls),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
